9eb4d9e8d39502e88482cf69caf0c6144b9848d0
yaowu <008623hyw@gmail.com>
2026-03-23 10:48:50 +0800

    求最小公倍数

---
+++ test-20260323/test-20260323/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//求最小公倍数
+int main() {
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	//方法1：暴力求解
+	//int m = (a > b ? a : b);    //最大公因数：将>改成<
+	//while (1) {
+	//	if (m % a == 0 && m % b == 0)    //最大公因数：将两个式子中的被除数和除数交换
+	//		break;
+	//	m++;    //最大公因数：m--;
+	//}
+	//printf("%d\n", m);
+	//方法2
+	int i = 1;
+	while (a * i % b)
+		i++;
+	printf("%d", i * a);
+	return 0;
+}
